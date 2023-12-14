01e017d5147d9e5f77064f862e2e0bf6deddbaff
Wilfried Ngassarou <125869257+wilfriedNgassarou@users.noreply.github.com>
2023-12-14 19:42:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const overlays = document.querySelectorAll('.overlay-after') ;
+
+let observer = new IntersectionObserver(([entrie]) => {
+  
+    if(entrie.isIntersecting) {
+
+      for(let child of entrie.target.children) {
+        const index = child.dataset.index ;
+      
+        child.style.transform = `scaleY(0)`;
+        child.style.transitionDelay = `${index * 200}ms` ;
+      }
+
+      observer.unobserve(entrie.target)
+      
+    }
+  
+}, { threshold: .5 })
+
+window.onload = function() {
+
+  // Retirer le loader dès que la page a chargé
+  document.querySelector('.loader').remove() ;
+  
+  for(let overlay of overlays) {
+    observer.observe(overlay)
+  }
+
+}
+
+
